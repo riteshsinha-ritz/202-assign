58181d0e333e978a369a09fe835afb9211163259
Ritesh Sinha <riteshsinha@MBP-Ritesh-Sinha-3.local>
2023-05-14 11:40:22 -0700

    Lint checker

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 202-assign
+
+lint_check.sh verifies all the input csv, json and xml files have proper structure.
+
+Required libraries for mac 
+brew install jq
+brew install csvkit
+brew install xmlstarlet
---
+++ lint_check.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+JSON_FILES=$(find . -name \*.json)
+for f in $JSON_FILES
+do
+  echo "Verifying $f file..."
+  jq '.' $f 1> /dev/null
+done
+echo $'All JSON files verified..\n'
+
+CSV_FILES=$(find . -name \*.csv)
+for f in $CSV_FILES
+do
+  echo "Verifying $f file..."
+  csvlook $f 1> /dev/null
+done
+echo $'All CSV files verified..\n'
+
+XML_FILES=$(find . -name \*.xml)
+for f in $XML_FILES
+do
+  echo "Verifying $f file..."
+  xmllint $f 1> /dev/null
+done
+echo $'All XML files verified..\n'
+
+echo "All input files lint check passed (SUCCESS)"
